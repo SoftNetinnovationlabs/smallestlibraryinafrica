6ed0c0f711573388633eab20ba25c4bb83027606
hackwellbn <oburasusan1@gmail.com>
2025-03-29 16:16:53 +0300

    adds navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,7 +4,21 @@ function Navbar() {
 
   return (
     <>
-    app under development  
+  <header>
+    <div className="header_container">
+    <div className="logo">smallestLibraryInAfrica</div>
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li className="links"><a href="HOME" className="link">HOME</a></li>
+        <li className="links"><a href="ABOUT US" className="link">ABOUT US</a></li>
+        <li className="links"><a href="OUR WORK" className="link">OUR WORK</a></li>
+        <li className="links"><a href="OUR IMPACT" className="link">OUR IMPACT</a></li>
+        <li className="links"><a href="GET INVOLVED" className="link">GET INVOLVED</a></li>
+      </ul>
+    </nav>
+    <div className="donate">DONATE</div>
+    </div>
+  </header>
     </>
   )
 }
