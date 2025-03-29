5f0c2ec02f4f39d180b7680a671b34d480b3773b
hackwellbn <oburasusan1@gmail.com>
2025-03-29 16:40:22 +0300

    adds navbar styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Home from './Pages/Home/Home'
+import {Routes, Route} from 'react-router-dom'
 import './App.css'
 
 const App = () => {
@@ -7,7 +9,15 @@ const App = () => {
   return (
     <>
       <div className="App">
-        <Navbar/>
+        <Navbar/> 
+        <Routes >
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<h1>About</h1>}/>
+          <Route path='/contact' element={<h1>Contact</h1>}/> 
+          <Route path='/login' element={<h1>Login</h1>}/>
+          <Route path='/register' element={<h1>Register</h1>}/>
+          <Route path='/dashboard' element={<h1>Dashboard</h1>}/>
+        </Routes>
         </div>
     </>
   )
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,26 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 function Navbar() {
-
   return (
     <>
-  <header>
-    <div className="header_container">
-    <div className="logo">smallestLibraryInAfrica</div>
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li className="links"><a href="HOME" className="link">HOME</a></li>
-        <li className="links"><a href="ABOUT US" className="link">ABOUT US</a></li>
-        <li className="links"><a href="OUR WORK" className="link">OUR WORK</a></li>
-        <li className="links"><a href="OUR IMPACT" className="link">OUR IMPACT</a></li>
-        <li className="links"><a href="GET INVOLVED" className="link">GET INVOLVED</a></li>
-      </ul>
-    </nav>
-    <div className="donate">DONATE</div>
-    </div>
-  </header>
+      <header>
+        <div className="header_container">
+          <div className="logo">logo</div>
+          <nav className="navbar">
+            <ul className="nav-links">
+              <li className="links">
+                <a href="HOME" className="link">
+                  HOME
+                </a>
+              </li>
+              <li className="links">
+                <a href="ABOUT US" className="link">
+                  ABOUT US
+                </a>
+              </li>
+              <li className="links">
+                <a href="OUR WORK" className="link">
+                  OUR WORK
+                </a>
+              </li>
+              <li className="links">
+                <a href="OUR IMPACT" className="link">
+                  OUR IMPACT
+                </a>
+              </li>
+              <li className="links">
+                <a href="GET INVOLVED" className="link">
+                  GET INVOLVED
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <Link to='/donate' className="btn">DONATE</Link>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
