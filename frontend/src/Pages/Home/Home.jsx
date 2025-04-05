0884af88c1c7f61a4ecdccb276380eef6da365c0
hackwellbn <oburasusan1@gmail.com>
2025-04-05 14:04:57 +0300

    adds hero image

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Vision from "./_Layouts/Vision/Vision.jsx";
 import TheNeed from "./_Layouts/TheNeed/TheNeed.jsx";
 import Hero from "./_Layouts/Hero/Hero.jsx";
+import Display from "./_Layouts/Display/Display";
 const Home = () => {
-  return <div>
-    <Hero />
-    <Vision />
-    <TheNeed />
-  </div>;
+  return (
+    <div>
+      <Hero />
+      <Vision />
+      <TheNeed />
+      <Display />
+    </div>
+  );
 };
 export default Home;
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,6 +1,8 @@
 import childrenModel from './r.webp'
 import education from './education.webp'
+import image1 from './IMG-0146.webp'
 export const assets = {
     childrenModel,
-    education
+    education,
+    image1
 };
