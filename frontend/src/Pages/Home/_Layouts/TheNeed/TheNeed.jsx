97b46134c30b68007545921a0425058a876b858c
hackwellbn <oburasusan1@gmail.com>
2025-04-06 00:38:54 +0300

    adds img

--- frontend/src/Pages/Home/_Layouts/TheNeed/TheNeed.jsx
+++ frontend/src/Pages/Home/_Layouts/TheNeed/TheNeed.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
-import './TheNeed.css'
+import React from "react";
+import "./TheNeed.css";
+import {assets} from '../../../../assets/assets'
 const TheNeed = () => {
   return (
-    <div className='theneed'>
-        <h1>The Need</h1>
+    <div className="theneed">
+      <h1>The Need</h1>
+      <div className="container__theneed">
+        <div className="theneed__content">
+          <div className="description">
+            <p>
+              {" "}
+              Majority of the Kenyan Urban population live in the informal slum
+              where access to basic human needs like food, shelter, clothing,
+              sanitation and education is a big challenge
+            </p>
+            <p>
+              {" "}
+              Children and Youths growing up in such settings are the most
+              vulnerable hence the continuous cycle of poverty one generation
+              after another.
+            </p>
+            <p>
+              {" "}
+              Baba dogo and lucky summer wards are such communities where
+              children and young adults are trapped by the systems that hold
+              urban poverty
+            </p>
+            <button className="btn">learn More</button>
+          </div>
+        </div>
+        <div className="theneed__content">
+          <div className="description">
+            <div className="img">
+              <img src={assets.image3} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TheNeed
+export default TheNeed;
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,8 +1,12 @@
 import childrenModel from './r.webp'
 import education from './education.webp'
 import image1 from './IMG-0146.webp'
+import image2 from './IMG-0162.webp'
+import image3 from './IMG-0246.webp'
 export const assets = {
     childrenModel,
     education,
-    image1
+    image1,
+    image2,
+    image3
 };
