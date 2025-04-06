6093bd64af5ba5161e8d44a6ea2cdec776ce9c5d
hackwellbn <oburasusan1@gmail.com>
2025-04-06 22:54:59 +0300

    adds icons in area focus

--- frontend/src/Pages/Home/_Layouts/AreaOfFocus/AreaOfFocus.jsx
+++ frontend/src/Pages/Home/_Layouts/AreaOfFocus/AreaOfFocus.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AreaOfFocus.css";
+import { Link } from "react-router-dom";
 const AreaOfFocus = () => {
   return (
     <div className="area_of_focus">
@@ -9,7 +10,7 @@ const AreaOfFocus = () => {
             <span className="material-symbols-outlined school">school</span>
           </div>
           <div className="body">
-            <h5>EDUCATION</h5>
+            <h4>EDUCATION</h4>
             <p>
               {" "}
               We promote literacy by promoting access to quality education and
@@ -27,15 +28,17 @@ const AreaOfFocus = () => {
             </span>
           </div>
           <div className="body">
-            <h6> HEALTH AND NUTRITION</h6>
+            <h4> HEALTH AND NUTRITION</h4>
             <p>
               {" "}
-              Wepromote proper growth and development of children below 5 years,
+              We promote proper growth and development of children below 5 years,
               pre adolescent and adolescent by establishing and supporting new
               and existing supplementary feeding programs at both community and
               schooling levels.Furthermore, we also promote access to clean
               water to schools and communities in informal settlements
             </p>
+            <Link to="/about" className="btn icons_btn"><span className="material-symbols-outlined">arrow_right_alt</span></Link>
+
           </div>
         </div>
         <div className="area__focus">
@@ -43,7 +46,7 @@ const AreaOfFocus = () => {
             <span className="material-symbols-outlined school" id="diversity">diversity_3</span>
           </div>
           <div className="body">
-            <h5> RESILIENT LIVELIHOODS</h5>
+            <h4> RESILIENT LIVELIHOODS</h4>
             <p>
               Facilitate the building of life and technical skills of youths
               ,tailored to current realities and market needs and link them to
