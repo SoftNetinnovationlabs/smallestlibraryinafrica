052f19d320d4a1398e598c40dc289d3130b687d4
hackwellbn <oburasusan1@gmail.com>
2025-04-06 21:44:29 +0300

    adds navbar phone

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,45 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <>
-      <header>
-        <div className="header_container">
-          <div className="logo">logo</div>
-          <nav className="navbar">
-            <ul className="nav-links">
-              <li className="links">
-                <Link to="/" className="link">
-                  HOME
-                </Link>
-              </li>
-              <li className="links">
-                <Link to="/about" className="link">
-                  ABOUT US
-                </Link>
-              </li>
-              <li className="links">
-                <Link to="/our-work" className="link">
-                  OUR WORK
-                </Link>
-              </li>
-              <li className="links">
-                <Link to="/our-impact" className="link">
-                  OUR IMPACT
-                </Link>
-              </li>
-              <li className="links">
-                <Link to="/register" className="link">
-                  GET INVOLVED
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <Link to='/donate' className="btn">DONATE</Link>
-        </div>
-      </header>
-    </>
+    <header>
+      <div className="header_container">
+        <div className="logo">logo</div>
+
+        <nav className={`navbar ${isOpen ? "open" : ""}`}>
+          {isOpen && (
+            <span className="material-symbols-outlined menu close" onClick={toggleMenu}>
+              close
+            </span>
+          )}
+          <ul className="nav-links">
+            <li className="links"><Link to="/" className="link" onClick={() => setIsOpen(false)}>HOME</Link></li>
+            <li className="links"><Link to="/about" className="link" onClick={() => setIsOpen(false)}>ABOUT US</Link></li>
+            <li className="links"><Link to="/our-work" className="link" onClick={() => setIsOpen(false)}>OUR WORK</Link></li>
+            <li className="links"><Link to="/our-impact" className="link" onClick={() => setIsOpen(false)}>OUR IMPACT</Link></li>
+            <li className="links"><Link to="/register" className="link" onClick={() => setIsOpen(false)}>GET INVOLVED</Link></li>
+          </ul>
+        </nav>
+
+        <Link to="/donate" className="btn nav_btn">DONATE</Link>
+
+        {!isOpen && (
+          <span className="material-symbols-outlined menu" onClick={toggleMenu}>
+            menu
+          </span>
+        )}
+      </div>
+    </header>
   );
 }
 
