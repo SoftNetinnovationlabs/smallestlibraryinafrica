0b357a70b3c85b6675293a454202b00fc571a453
hackwellbn <oburasusan1@gmail.com>
2025-05-02 18:10:33 +0300

    adds backend

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://api.smallestlibrary.onrender.com' // Production URL
+    ? 'https://api-smallestlibraryinafrica.onrender.com' // Production URL
     : process.env.NODE_ENV === 'staging'
-    ? 'https://api.smallestlibrary.org' // Staging URL
+    ? 'https://api.smallestlibraryinafrica.org' // Staging URL
     : 'http://localhost:5000/api'; // Development URL
     export default BASE_URL
