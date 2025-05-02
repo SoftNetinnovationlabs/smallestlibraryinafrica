6fbdb5a36709ebb2bbcfa9c9299d6994ef74a3e1
hackwellbn <oburasusan1@gmail.com>
2025-05-02 19:52:58 +0300

    c is small letter for context in main

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://api-smallestlibraryinafrica.onrender.com.api' // Production URL
+    ? 'https://api-smallestlibraryinafrica.onrender.com/api' // Production URL
     : process.env.NODE_ENV === 'staging'
     ? 'https://api.smallestlibraryinafrica.org/api' // Staging URL
     : 'http://localhost:5000/api'; // Development URL
