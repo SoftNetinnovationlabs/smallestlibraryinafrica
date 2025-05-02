82f46c15b977163576c7b9538b87a273a4aa83a6
hackwellbn <oburasusan1@gmail.com>
2025-05-02 18:08:15 +0300

    adds backend

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {AuthController} from '../controllers/auth.controller.js'
+import {AuthController} from '../controllers/Auth.controller.js'
 const AuthRoutes = express.Router();
 
 AuthRoutes.post('/register', AuthController)
