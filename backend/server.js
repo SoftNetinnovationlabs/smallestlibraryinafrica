dd23c1e730cb0183a3475dfed243f9d7324c8b62
hackwellbn <oburasusan1@gmail.com>
2025-05-03 21:52:54 +0300

    adds admin link

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import NewsletterRoutes from './routes/newsletter.routes.js'
 dotenv.config();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://admin.smallestlibraryinafrica.org' 'https://smallestlibraryinafrica.org'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://admin.smallestlibraryinafrica.org' ,'https://smallestlibraryinafrica.org'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
