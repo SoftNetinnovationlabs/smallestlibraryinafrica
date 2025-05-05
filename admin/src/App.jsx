b0cfea896d9d8ba7c5a40907705f4df10edefa96
hackwellbn <oburasusan1@gmail.com>
2025-05-05 21:53:33 +0300

    adds auth

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,11 +4,13 @@ import DashboardLayout from './Pages/Dashboard/DashboardLayout';
 import DashboardHome from './Pages/Dashboard/DashboardHome';
 import Profile from './Pages/Dashboard/Profile';
 import NewsLetterSender from './Pages/Dashboard/NewsLetterSender';
+import Auth from './Auth/auth'
 import './App.css';
 
 const App = () => {
   return (
     <Routes>
+      <Route path='/login' element={<Auth />} />
       <Route path="/" element={<DashboardLayout />}>
         <Route index element={<DashboardHome />} />
         <Route path="profile" element={<Profile />} />
