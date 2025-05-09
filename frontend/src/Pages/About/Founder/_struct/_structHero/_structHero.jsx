b0d3ab254ef69923eb5c455f2f2d903bd65247b4
hackwellbn <oburasusan1@gmail.com>
2025-05-09 19:09:11 +0300

    adds text

--- frontend/src/Pages/About/Founder/_struct/_structHero/_structHero.jsx
+++ frontend/src/Pages/About/Founder/_struct/_structHero/_structHero.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import {assets} from '../../../../../assets/assets'
-import './_structHero.css'
+import React from 'react';
+import { assets } from '../../../../../assets/assets';
+import './_structHero.css';
+
 const _structHero = () => {
   return (
     <div className='struct-hero'>
-        <div className='struct-hero-container'>
+      <div className='struct-hero-container'>
         <div className='hero__-content'>
-            <div className='hero-image'>
-                <img src={assets.Founder} />
-            </div>
+          <div className='hero-image'>
+            <img src={assets.Founder} alt="Founder Cyril Peter Odhiambo" />
+          </div>
         </div>
         <div className='hero__content'>
-            <h1>about Our Founder</h1>
-            <h5>Cyril Peter Odhiambo</h5>
+          <h1>About Our Founder</h1>
+          <h4>Cyril Peter Odhiambo</h4>
+          <h5>A Story of Resilience: The Journey of Cyril Otieno and the Smallest Library in Africa</h5>
+          <div className='p_graph'>
+            <p>
+              Cyril Otieno’s story is one of courage, perseverance, and an unyielding commitment to his community.
+              Growing up in the Mugure slums of Kenya, Cyril faced a myriad of socio-economic challenges that could
+              have easily deterred him from dreaming of a better future. Yet, it was these very hardships that fueled
+              his determination to create change.
+            </p>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default _structHero
+export default _structHero;
