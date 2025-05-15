61857ac00050df77aafd3f0350c1340aec8d0066
hackwellbn <oburasusan1@gmail.com>
2025-05-15 20:16:51 +0300

    removed text for our work

--- frontend/src/Pages/OurWork/_Layouts/HeroWork/HeroWork.jsx
+++ frontend/src/Pages/OurWork/_Layouts/HeroWork/HeroWork.jsx
@@ -5,7 +5,6 @@ const HeroWork = () =>{
         <div>
             <div className="hero-work">
                 <h1>Our Work</h1>
-            <p>At <span className="approach__highlight">The Childrens Voice</span>, we believe that every child deserves a chance to thrive. Our approach is centered around empowering children and their families to break the cycle of poverty and create a brighter future.</p>                
                 </div>
         </div>
     )
