740530ce931560963898c2c9b2005191fd176931
hackwellbn <oburasusan1@gmail.com>
2025-05-22 19:44:44 +0300

    adds panel

--- admin/src/Pages/Dashboard/SideBar/SideBar.jsx
+++ admin/src/Pages/Dashboard/SideBar/SideBar.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import './Sidebar.css'; // optional: for styling
 
 const SideBar = () => {
+  const [isOpen , setIsOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
-    <div className="sidebar">
-      <h2 className="sidebar-title">Admin Panel</h2>
+    <div className="sidebar" style={{ width: isOpen ? '200px' : '50px' }}>
+      <button className="toggle-button" onClick={toggleSidebar}>
+        {isOpen ? 'Close' : 'Open'}
+      </button>
+      <h2 className="sidebar-title">{isOpen ? 'Admin': "A"}</h2>
       <nav className="sidebar-nav">
         <NavLink to="/" className="sidebar-link" end>
-          Dashboard Home
+                {isOpen ? 'Dashboard Home' : 'D'}
+
         </NavLink>
         <NavLink to="/profile" className="sidebar-link">
-          Profile
+        {isOpen ? 'Profile' : 'P'}
         </NavLink>
         <NavLink to="/sendNewsletters" className="sidebar-link">
-          Send Newsletters
+      {isOpen ? 'Send Newsletters' : 'S'}
         </NavLink>
         <NavLink to="/news" className="sidebar-link">
-          News
+          {isOpen ? 'News' : 'N'}
         </NavLink>
       </nav>
     </div>
