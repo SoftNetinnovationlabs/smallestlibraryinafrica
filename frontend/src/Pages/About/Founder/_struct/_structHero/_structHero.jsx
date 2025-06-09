eb21e6d3a41dd0b98440f86dcc4abb4b4f9a5a7f
hackwellbn <oburasusan1@gmail.com>
2025-06-09 12:09:24 +0300

    start of board development

--- frontend/src/Pages/About/Founder/_struct/_structHero/_structHero.jsx
+++ frontend/src/Pages/About/Founder/_struct/_structHero/_structHero.jsx
@@ -1,71 +1,92 @@
-import React from 'react';
-import { assets } from '../../../../../assets/assets';
-import './_structHero.css';
-import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
-import { FaXTwitter } from 'react-icons/fa6';
+import React from "react";
+import { assets } from "../../../../../assets/assets";
+import "./_structHero.css";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
-const FounderData = [{
-  description: `
+const FounderData = [
+  {
+    description: `
   Cyril’s early life was marked by constant struggle. He was often sent home from school because his family couldn’t afford the fees. At times, even necessities like food were scarce. To survive, Cyril and his peers resorted to scavenging for scrap metal to sell. Life became even more precarious as he entered adolescence, with many 
-  of his friends turning to crime, lured by the promise of a flashy lifestyle. Tragically, this path often led to fatal outcomes, with young lives lost to mob justice or police violence.`
-},{
-description: `Amid this chaos, Cyril’s resilience began to shine. In 2011, while still in high school, he founded a garbage collection group. The initiative not only provided a modest income for him and his peers but also instilled in him the power of collective action. Cyril used his earnings to pay his school fees, ultimately graduating in 2012. This project was a turning point, sparking a realization: children and youth in his community needed alternatives to the destructive paths so many were forced to take.`
-},{
-  description: `In 2013, Cyril’s leadership potential earned him a scholarship to study Community Development and Project Planning at the Regional Institute of Business Management. Armed with newfound knowledge and skills, Cyril returned to his roots, determined to make a lasting impact.`
-},
-{
-  description: `At just 21 years old, in July 2014, Cyril identified a simple yet powerful way to address one of his community’s critical needs: education. He established the Smallest Library in Africa, starting with an open-air space to provide children with access to books and a quiet place to read. This initiative was more than just a library; it was a beacon of hope, symbolizing the transformative power of education.`
-},{
-  description: `
-  Over the years, the Smallest Library in Africa has evolved into a literacy powerhouse. What began as a humble collection of books has grown into a movement that conducts book drives and establishes libraries in remote schools and underserved communities. Beyond improving literacy, the library has united the community, demonstrating that lasting change is possible when people come together.`
-},{
-  description: `
-  Today, the Smallest Library in Africa continues to inspire. It not only provides educational resources but also fosters a sense of possibility and pride among the children and youth it serves. Cyril’s journey from a struggling boy in the slums to a community leader and changemaker is a testament to the resilience of the human spirit and the power of education to transform lives.`
-}
-]
+  of his friends turning to crime, lured by the promise of a flashy lifestyle. Tragically, this path often led to fatal outcomes, with young lives lost to mob justice or police violence.`,
+  },
+  {
+    description: `Amid this chaos, Cyril’s resilience began to shine. In 2011, while still in high school, he founded a garbage collection group. The initiative not only provided a modest income for him and his peers but also instilled in him the power of collective action. Cyril used his earnings to pay his school fees, ultimately graduating in 2012. This project was a turning point, sparking a realization: children and youth in his community needed alternatives to the destructive paths so many were forced to take.`,
+  },
+  {
+    description: `In 2013, Cyril’s leadership potential earned him a scholarship to study Community Development and Project Planning at the Regional Institute of Business Management. Armed with newfound knowledge and skills, Cyril returned to his roots, determined to make a lasting impact.`,
+  },
+  {
+    description: `At just 21 years old, in July 2014, Cyril identified a simple yet powerful way to address one of his community’s critical needs: education. He established the Smallest Library in Africa, starting with an open-air space to provide children with access to books and a quiet place to read. This initiative was more than just a library; it was a beacon of hope, symbolizing the transformative power of education.`,
+  },
+  {
+    description: `
+  Over the years, the Smallest Library in Africa has evolved into a literacy powerhouse. What began as a humble collection of books has grown into a movement that conducts book drives and establishes libraries in remote schools and underserved communities. Beyond improving literacy, the library has united the community, demonstrating that lasting change is possible when people come together.`,
+  },
+  {
+    description: `
+  Today, the Smallest Library in Africa continues to inspire. It not only provides educational resources but also fosters a sense of possibility and pride among the children and youth it serves. Cyril’s journey from a struggling boy in the slums to a community leader and changemaker is a testament to the resilience of the human spirit and the power of education to transform lives.`,
+  },
+];
 
 const _structHero = () => {
   return (
-    <div className='struct-hero'>
-      <div className='struct-hero-container'>
-        <div className='hero__-content'>
-          <div className='hero-image'>
+    <div className="struct-hero">
+      <div className="struct-hero-container">
+        <div className="hero__-content">
+          <div className="hero-image">
             <img src={assets.Founder} alt="Founder Cyril Peter Odhiambo" />
           </div>
         </div>
-        <div className='hero__-content'>
+        <div className="hero__-content">
           <h1>About Our Founder</h1>
           <h4>Cyril Peter Odhiambo</h4>
-          <h5>A Story of Resilience: The Journey of Cyril Otieno and the Smallest Library in Africa</h5>
-          <div className='p_graph'>
+          <h5>
+            A Story of Resilience: The Journey of Cyril Otieno and the Smallest
+            Library in Africa
+          </h5>
+          <div className="p_graph">
             <p>
-              Cyril Otieno’s story is one of courage, perseverance, and an unyielding commitment to his community.
-              Growing up in the Mugure slums of Kenya, Cyril faced a myriad of socio-economic challenges that could
-              have easily deterred him from dreaming of a better future. Yet, it was these very hardships that fueled
-              his determination to create change.
+              Cyril Otieno’s story is one of courage, perseverance, and an
+              unyielding commitment to his community. Growing up in the Mugure
+              slums of Kenya, Cyril faced a myriad of socio-economic challenges
+              that could have easily deterred him from dreaming of a better
+              future. Yet, it was these very hardships that fueled his
+              determination to create change.
             </p>
-            <div className='social-links'>
-              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <div className="social-links">
+              <a
+                href="https://facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <FaFacebookF />
               </a>
-              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <FaXTwitter />
               </a>
-              <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://linkedin.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <FaLinkedinIn />
               </a>
             </div>
           </div>
         </div>
-       <div className='text_container'>
-      {FounderData.map((texts, index)=>(
-        <div className="paragraph" key={index}>
-          <p>{texts.description}</p>
-        </div>
-      ))}
+        <div className="text_container">
+          {FounderData.map((texts, index) => (
+            <div className="paragraph" key={index}>
+              <p>{texts.description}</p>
+            </div>
+          ))}
         </div>
       </div>
-      
     </div>
   );
 };
--- frontend/src/Pages/About/_Layouts/Story/Story.jsx
+++ frontend/src/Pages/About/_Layouts/Story/Story.jsx
@@ -6,81 +6,78 @@ import { assets } from "../../../../assets/assets";
 const Story = () => {
   return (
     <div>
-    <div className="story__wrapper">
-<div className="text-story">       
-   <h3>Our Story: the humble beginnings</h3>
-</div>
-      <div className="story__container">
-        <div className="story__content">
-          <p>
-            The Smallest Library In Africa Initiative began as an open-air library in Mugure slums, founded by Cyril Peter Otieno. The library operated five days a week from 5:30 pm to 7:30 pm, closing during the short and long rainy seasons.
-          </p>
+      <div className="story__wrapper">
+        <div className="text-story">
+          <h3>Our Story: the humble beginnings</h3>
         </div>
-
-          <p>
-            Despite its humble beginnings, the library became a vital learning space for school-going children, offering access to books and a safe, supportive environment—something previously unimaginable in the area.
-          </p>
-
-          <p>
-            Having experienced similar educational and social challenges, Cyril believed in the potential of his community and was determined to transform their narrative from one of impossibilities to possibilities.
-          </p>
-
-          <p>
-            Today, the Smallest Library In Africa is part of a global movement to end inequality by promoting education, nutrition, and sustainable livelihoods in marginalized communities. Our efforts are powered by technology and innovation to design and implement impactful solutions.
-          </p>
-      </div>
-
-    </div>
-     <div className='team' style={{textAlign: 'center', padding: '100px 0'}}>
-      <h1 >Our Team </h1>
-       <section className="about__container">
-        <div className="about__card">
-          <div className="founder_people">
-            <img src={assets.Cyril} alt="Founder" />
-          </div>
-          <div className="about__content">
-            <h4>Our Founder</h4>
-            <h5>Cyril Peter Otieno</h5>
-            {/* <p>
-              Meet the visionary behind the initiative—a young entrepreneur passionate about transforming communities through education and opportunity.
-            </p> */}
-            <Link className="link" to="/about/founder">
-              <FaArrowRight />
-            </Link>
-          </div>
-        </div>
-
-        <div className="about__card">
-          <div className="about__content">
-               <div className="founder_people">
-            <img src={assets.board} alt="Founder" />
-          </div>
-            <h4>Our Board of Directors</h4>
-            {/* <p>
-              Our board is composed of experienced leaders dedicated to advancing our mission and ensuring transparency, accountability, and sustainability.
-            </p> */}
-            <Link className="link" to="/about/board-of-directors">
-              <FaArrowRight />
-            </Link>
-          </div>
-        </div>
-
-        {/* <div className="about__card">
-          <div className="about__content">
-               <div className="founder_people">
-            <img src={assets.Founder} alt="Founder" />
-          </div>
-            <h4>Our Staff</h4>
+        <div className="story__container">
+          <div className="story__content">
             <p>
-              Meet the dedicated team members who work tirelessly to bring our programs and initiatives to life.
+              The Smallest Library In Africa Initiative began as an open-air
+              library in Mugure slums, founded by Cyril Peter Otieno. The
+              library operated five days a week from 5:30 pm to 7:30 pm, closing
+              during the short and long rainy seasons.
             </p>
-            <Link className="link" to="/about/staff">
-              <FaArrowRight />
+          </div>
+
+          <p>
+            Despite its humble beginnings, the library became a vital learning
+            space for school-going children, offering access to books and a
+            safe, supportive environment—something previously unimaginable in
+            the area.
+          </p>
+
+          <p>
+            Having experienced similar educational and social challenges, Cyril
+            believed in the potential of his community and was determined to
+            transform their narrative from one of impossibilities to
+            possibilities.
+          </p>
+
+          <p>
+            Today, the Smallest Library In Africa is part of a global movement
+            to end inequality by promoting education, nutrition, and sustainable
+            livelihoods in marginalized communities. Our efforts are powered by
+            technology and innovation to design and implement impactful
+            solutions.
+          </p>
+        </div>
+      </div>
+      <div className="team improved-team-section">
+        <h1 className="improved-team-title">Our Team</h1>
+        <section className="about__container improved-team-grid">
+          <div className="about__card improved-card">
+            <div className="founder_people improved-img-wrapper">
+              <img src={assets.Cyril} alt="Founder" />
+            </div>
+            <Link style={{textDecoration: 'none'}} to={'/about/founder'} className="about__content">
+              <h4>Our Founder</h4>
+              <h5>Cyril Peter Otieno</h5>
+              <p className="team-desc">
+                Meet the visionary behind the initiative—a young entrepreneur
+                passionate about transforming communities through education and
+                opportunity.
+              </p>
+            
             </Link>
           </div>
-        </div> */}
-      </section>
-     </div>
+
+          <div className="about__card improved-card">
+            <div className="founder_people improved-img-wrapper">
+              <img src={assets.board} alt="Board of Directors" />
+            </div>
+            <Link style={{textDecoration: 'none'}} to={'/about/board-of-directors'} className="about__content">
+              <h4>Our Board of Directors</h4>
+              <p className="team-desc">
+                Our board is composed of experienced leaders dedicated to
+                advancing our mission and ensuring transparency, accountability,
+                and sustainability.
+              </p>
+          </Link>
+            
+            </div>
+        </section>
+      </div>
     </div>
   );
 };
