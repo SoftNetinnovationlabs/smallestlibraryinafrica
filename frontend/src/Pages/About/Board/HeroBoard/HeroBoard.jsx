aa1fffe302e50f30ceeb978153b5c03c4b6a3ea8
hackwellbn <oburasusan1@gmail.com>
2025-06-12 14:48:21 +0300

    adds more

--- frontend/src/Pages/About/Board/HeroBoard/HeroBoard.jsx
+++ frontend/src/Pages/About/Board/HeroBoard/HeroBoard.jsx
@@ -3,6 +3,9 @@ import "./HeroBoard.css";
 const HeroBoard = () => {
   return (
     <div className="hero-board">
+      <div className="hero-board_c">
+        <h1>Meet Our Board of Directors</h1>
+      </div>
       <div className="overlay_bottom-left">
         <p>
           meeet the behind the scenes, the driving force of our organization
